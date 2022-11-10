a1e804091143b86d22bce23df6f396884a130ca6
Dominic Arrojado <dominicarrojado@gmail.com>
2022-11-10 14:24:26 +0800

    create BarGraph component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import BarGraph from './components/BarGraph';
 
 export default function App() {
   return (
     <div className="container">
       <h1>React TypeScript Bar Graph</h1>
+      <BarGraph />
     </div>
   );
 }
--- src/components/BarItem.tsx
+++ src/components/BarItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './BarItem.css';
 
 export default function BarItem() {
